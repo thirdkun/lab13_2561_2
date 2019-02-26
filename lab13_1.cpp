3ac82e3b20a62cbf18a174f739dc5d632bad6e86
lab303-14 <lab303-14@labeng.local>
2019-02-26 18:19:04 +0700

    ข้อ 1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 template <typename T>
+void print(T d[],int E,int Q){
+	for(int i=0;i<E;i++){
+		if(i==Q){
+			cout << "[" << d[i] << "]" << " ";
+		}else cout << d[i] << " ";
+	}
+}
+template <typename T>
 void swap(T d[],int x,int y){
     T temp = d[x];
     d[x] = d[y];
@@ -11,7 +19,18 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
-	
+	int j;
+	for(int i=1;i<N;i++){
+		print(d,N,i);
+		cout << "=> "; 
+		for(j=i;j>0;j--){
+			if(d[j]>d[j-1]){
+				swap(d,j,j-1); 
+                }
+				else break;
+		}print(d,N,j);
+		cout << "\n";
+	}
 //Write your code here.
 
 }
