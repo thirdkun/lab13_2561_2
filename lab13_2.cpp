24a3c5af8955ac500e7c65de4f7e50c603e4aa2c
lab303-14 <lab303-14@labeng.local>
2019-02-26 18:19:14 +0700

    ข้อ 2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -11,7 +11,7 @@ void showMatrix(const bool [][N]);
 
 int main(){
 	double A[N][N]; 
-	bool B[N][N]; 
+	bool B[N][N]={0}; 
 	cout << "Input the matrix.....\n";
 	inputMatrix(A);
 	findLocalMax(A,B);
@@ -19,5 +19,31 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void inputMatrix(double A[][N]){
+	for(int i=0;i<N;i++){
+		cout << "Row " << i+1 << ": ";
+		for(int j=0;j<N;j++){
+			cin >> A[i][j];
+		}
+	}
+}
+void findLocalMax(const double A[][N], bool B[][N]){
+	for(int i=1;i<N-1;i++){
+		for(int j=1;j<N-1;j++){
+			if(A[i][j] > A[i-1][j] && A[i][j] > A[i+1][j] && A[i][j] > A[i][j-1] && A[i][j] > A[i][j+1]){
+				B[i][j] = true;
+			}
+			else B[i][j] = false;
+		}
+	}
+}
+void showMatrix(const bool B[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			cout << B[i][j] << " ";
+		}
+	    cout << "\n";
+	}
+}
 
-// Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
+		// Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
